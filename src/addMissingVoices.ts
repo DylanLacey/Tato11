1e2f2b2e2b51a063df84b3da6340ad24120727a9
The Gentlehacker <github@dylanlacey.com>
2024-02-01 14:25:12 +1000

    Refactored to add defaults, cleaner API

--- src/addMissingVoices.ts
+++ src/addMissingVoices.ts
@@ -1,15 +1,82 @@
-
-const voice_id = "ErXwobaYiN019PkySvjV"
+const defaultVoice = "IKne3meq5aSn9XLyUdCD"
+const defaultText = "Mate! You forgot to provide a text argument!"
 const apiKey = "c52d67f158423efa139aa506780a72c7"
 
 const sample_rate = 24000
 
-export const streamAudio = async () => {
-    await playResponse(await doFetch("久しぶり、だな", voice_id))
+// export const streamAudio = async (text: string, voice_id: string) => {
+//     await playResponse(await doFetch("久しぶり、だな", voice_id))
+// }
+
+// export const retrieveAudioData = async (text: string, voice_id: string) => {
+//     return getResponse(await doFetch("久しぶり、だな", voice_id))
+// }
+
+export const streamAudio = async (text: string = defaultText, voice_id:string = defaultVoice) => {
+    await playResponse(await doFetch(text, voice_id))
 }
 
-export const getAudioData = async () => {
-    return getResponse(await doFetch("久しぶり、だな", voice_id))
+export const retrieveAudioData = async (text: string = defaultText, voice_id: string = defaultVoice) => {
+    return getResponse(await doFetch(text, voice_id))
+}
+
+export const streamAndRetrieveAudioData = async (text: string = defaultText, voice_id: string = defaultVoice) => {
+    const apiResponse = await doFetch(text, voice_id)
+
+    const {audioDataProducer, audioDataChunkHandler} = storeHandlers()
+    const audioPlayChunkHandler = playHandler()
+
+    const doboth = (chunkData: Float32Array) => {
+        audioPlayChunkHandler(chunkData)
+        audioDataChunkHandler(chunkData)
+    }
+
+    await processStream(apiResponse, doboth)
+
+    return audioDataProducer()
+}
+
+const playHandler = (context: AudioContext = new AudioContext(), delay: number = 0) => {
+    let startAt = context.currentTime + delay;
+
+    return (chunkData: Float32Array) => {
+        const processingBuffer = context.createBuffer(1, chunkData.length, sample_rate)
+        processingBuffer.copyToChannel(chunkData, 0)
+
+        const nowBufferingSource = context.createBufferSource()
+        nowBufferingSource.buffer = processingBuffer
+        nowBufferingSource.connect(context.destination)
+
+        nowBufferingSource.start(startAt)
+        startAt += processingBuffer.duration
+    }
+}
+
+const storeHandlers = () => {
+    const chunksArray = new Array()
+    let totalBytes = 0
+
+    const audioDataChunkHandler = (chunkData: Float32Array) => {
+        chunksArray.push({data:chunkData, size: chunkData.length})
+        totalBytes += chunkData.length
+    }
+
+    const audioDataProducer = () => {      
+        let responseData = new Float32Array(totalBytes)
+        
+        responseData = chunksArray.reduce((response, chunk) => { 
+            response.data.set(chunk.data, response.offset)
+            response.offset += chunk.size
+    
+            return response
+        },
+            {data: responseData, offset: 0}
+        ).data
+    
+        return responseData
+    }
+
+    return {audioDataProducer, audioDataChunkHandler}
 }
 
 const getResponse = async (response: Response) => {
@@ -36,9 +103,9 @@ const getResponse = async (response: Response) => {
     return responseData
 }
 
-const playResponse = async (response: Response) => {
+const playResponse = async (response: Response, delay: number = 0) => {
     const context = new AudioContext();
-    let startAt = context.currentTime;
+    let startAt = context.currentTime + delay;
 
     const play = (chunkData: Float32Array) => {
         const processingBuffer = context.createBuffer(1, chunkData.length, sample_rate)
@@ -90,60 +157,7 @@ export const processStream = async (response: Response, fn: Function) => {
 
         readingStream = !(chunk?.done)
     }
-}
-
-// export const streamAudio = async (text: string, voice_id: string) => {
-//     const stream = await doFetch(text, voice_id)
-//     const streamReader = stream.body?.getReader()
-
-//     const context = new AudioContext();
-//     let startAt = context.currentTime;
-
-//     let leftover: number | undefined;
-
-//     let readingStream = true
-//     while (readingStream) {
-//         const chunk = await streamReader?.read()
-        
-//         if (chunk?.value) {
-//             let chunkBytes: Uint8Array
-
-//             if (leftover) {
-//                 chunkBytes = new Uint8Array([leftover, ...chunk.value])
-//                 leftover = undefined
-//             } else {
-//                 chunkBytes = chunk.value
-//             }
-
-            // if (chunkBytes.byteLength % 2 == 1) {
-            //     leftover = chunkBytes[chunkBytes.byteLength - 1];
-            // }
-
-    //         const processingBuffer = context.createBuffer(1, chunkBytes.byteLength / 2, sample_rate)
-
-    //         let dataView = new DataView(chunkBytes.buffer)
-    //         let channelData = new Float32Array(chunkBytes.byteLength / 2)
-
-    //         for (let i = 0; i < Math.floor(dataView.byteLength / 2) * 2; i+= 2) {
-    //             const sample = dataView.getInt16(i, true)
-    //             let floatSample = sample / 32768
-    //             channelData[i/2] = floatSample
-    //         }
-            
-    //         processingBuffer.copyToChannel(channelData, 0)
-
-    //         const nowBufferingSource = context.createBufferSource()
-    //         nowBufferingSource.buffer = processingBuffer
-    //         nowBufferingSource.connect(context.destination)
-
-    //         console.debug(`Chunk of duration ${processingBuffer.duration} starts at ${startAt}`)
-    //         nowBufferingSource.start(startAt)
-    //         startAt += processingBuffer.duration
-    //     }
-
-    //     readingStream = !(chunk?.done) 
-    // }   
-// }
+}    
 
 const doFetch = async (text: string, voice_id: string) => {
     console.log("Making request")
@@ -162,75 +176,4 @@ const doFetch = async (text: string, voice_id: string) => {
             text: text,
         })
     })
-}
-
-export const fetchAudio = async () => {
-    const url = new URL(`https://api.elevenlabs.io/v1/text-to-speech/${voice_id}/stream`)
-    url.searchParams.append("optimize_streaming_latency", "2")
-    url.searchParams.append("output_format", "pcm_24000")
-
-    const stream = await fetch(url, {
-        method: "POST",
-        headers: {
-            "Content-Type": "application/json",
-            "xi-api-key": apiKey
-        },
-        body: JSON.stringify({
-            model_id: "eleven_multilingual_v2",
-            text: "久しぶり、だな",
-        })
-    })
-
-    const streamReader = stream.body?.getReader()
-    let readingStream = true
-
-    let totalBytes = 0;
-    const context = new AudioContext();
-    let startAt = context.currentTime;
-
-    let leftover: number | undefined;
-    while (readingStream) {
-        const chunk = await streamReader?.read()
-        
-        if (chunk?.value) {
-            let chunkBytes: Uint8Array
-
-            if (leftover) {
-                chunkBytes = new Uint8Array([leftover, ...chunk.value])
-                leftover = undefined
-            } else {
-                chunkBytes = chunk.value
-            }
-
-            if (chunkBytes.byteLength % 2 == 1) {
-                leftover = chunkBytes[chunkBytes.byteLength - 1];
-            }
-
-            totalBytes += chunkBytes.byteLength
-            const processingBuffer = context.createBuffer(1, chunkBytes.byteLength / 2, sample_rate)
-
-            let dataView = new DataView(chunkBytes.buffer)
-            let channelData = new Float32Array(chunkBytes.byteLength / 2)
-
-            for (let i = 0; i < Math.floor(dataView.byteLength / 2) * 2; i+= 2) {
-                const sample = dataView.getInt16(i, true)
-                let floatSample = sample / 32768
-                channelData[i/2] = floatSample
-            }
-            
-            processingBuffer.copyToChannel(channelData, 0)
-
-            const nowBufferingSource = context.createBufferSource()
-            nowBufferingSource.buffer = processingBuffer
-            nowBufferingSource.connect(context.destination)
-
-            console.debug(`Chunk of duration ${processingBuffer.duration} starts at ${startAt}`)
-            nowBufferingSource.start(startAt)
-            startAt += processingBuffer.duration
-        }
-
-        readingStream = !(chunk?.done) 
-    }   
-
-    console.debug("Total data read: " + totalBytes)
 }
--- src/pages/Popup.svelte
+++ src/pages/Popup.svelte
@@ -1,15 +1,18 @@
 <script lang="ts">
   console.log("Hello from the popup!");
 
-  import { streamAudio, fetchAudio } from "../addMissingVoices";
+  import { streamAudio, streamAndRetrieveAudioData } from "../addMissingVoices";
   import { Button } from "flowbite-svelte";
+
+  const doVoice = async () => {
+    streamAudio()
+  }
 </script>
 
 <div>
   <img src="/icon-with-shadow.svg" alt="" />
   <h1>vite-plugin-web-extension</h1>
-  <Button on:click={streamAudio} color="primary" size="lg">Refactored</Button>
-  <Button on:click={fetchAudio} color="primary" size="lg">Old</Button>
+  <Button on:click={doVoice} color="primary" size="lg">Test Audio</Button>
   <p>
     Template: <code>svelte-ts</code>
   </p>
